eef5e65e9247b44b5371c6a205bf91dd30146eec
Sofiane0308 <48534055+Sofiane0308@users.noreply.github.com>
2020-02-17 00:08:43 +0100

    final

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -7,26 +7,26 @@ import * as middy from 'middy';
 
 const logger = createLogger('auth')
 
-const cert = `-----BEGIN CERTIFICATE-----
-MIIDDTCCAfWgAwIBAgIJe0S559ZhEbmyMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
-BAMTGWRldi0xdWhxbnV0YS5ldS5hdXRoMC5jb20wHhcNMjAwMjE2MTIwMTQ5WhcN
-MzMxMDI1MTIwMTQ5WjAkMSIwIAYDVQQDExlkZXYtMXVocW51dGEuZXUuYXV0aDAu
-Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7+W4NOrJSgagHPvW
-7cxtXK0GRft1wxpdlEG6xTq6KSRPWXVXY3zuEBp6JL5LOatbIIJyyulpIun1LdQQ
-Nfic5xcvSOaqrpXsB+qZMsJkitjgNt1/azhEGvrtxSwVhwtFcPXTprvRqCWdB38f
-9YwoPNb8L57xmZIDBGJ7z4NdRFxbHJw4TW3B5KG4hRUmDxjBk6z1+DNrHqmpK2An
-Z9qaLOFS+V7ZONml7fAYwgjDO6Uqt7LD3m5VbC6wrNf0jTxyJlQ/b2S3U/TVU6OS
-K4Qc79yJ9CFTHccDGifsHgozAKKtotMx81bIRXwa5F5x0xGa/qbS3SXFaDgpFYu8
-YUpsVwIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBR8l3xQe6P+
-DteHkVtqa7kYLhwIcjAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
-ACTZrmaoV1X1nkR7/PtvuG7jIqBGOaFkjMSid6HDMkTy4qCvHPCB/0X6/7Ti2PhR
-HU7+dYmyDj7/8nfQanjfI23SYqhMWGsqzqRvXOZWBlTLfPGDOM01dOiuGGU/gjtB
-XcuvV8qOLghTJjjb11ye5YrLdd7RcnN+yRaQV3Z6OhDhuzNDoT83gLn1AuqzTVgA
-YXWhc/O1OqvYtxS3bMKKhSbo6bRTTnOgnBFKpye9VwKx5rNc6RD8ztAScHiZAR3p
-xN8YaXAdomDJR4HZpXcJ5gCy3rT+5PvfaCNZ/pKfF8RFbgr/VulmsjspglDLa6t0
-4VaP+Pxcgr4AjEii2yxksGg=
------END CERTIFICATE-----
-`
+const cert = '-----BEGIN CERTIFICATE-----\
+MIIDDTCCAfWgAwIBAgIJe0S559ZhEbmyMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV\
+BAMTGWRldi0xdWhxbnV0YS5ldS5hdXRoMC5jb20wHhcNMjAwMjE2MTIwMTQ5WhcN\
+MzMxMDI1MTIwMTQ5WjAkMSIwIAYDVQQDExlkZXYtMXVocW51dGEuZXUuYXV0aDAu\
+Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7 + W4NOrJSgagHPvW\
+7cxtXK0GRft1wxpdlEG6xTq6KSRPWXVXY3zuEBp6JL5LOatbIIJyyulpIun1LdQQ\
+Nfic5xcvSOaqrpXsB + qZMsJkitjgNt1 / azhEGvrtxSwVhwtFcPXTprvRqCWdB38f\
+9YwoPNb8L57xmZIDBGJ7z4NdRFxbHJw4TW3B5KG4hRUmDxjBk6z1 + DNrHqmpK2An\
+Z9qaLOFS + V7ZONml7fAYwgjDO6Uqt7LD3m5VbC6wrNf0jTxyJlQ / b2S3U / TVU6OS\
+K4Qc79yJ9CFTHccDGifsHgozAKKtotMx81bIRXwa5F5x0xGa / qbS3SXFaDgpFYu8\
+YUpsVwIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH / MB0GA1UdDgQWBBR8l3xQe6P +\
+  DteHkVtqa7kYLhwIcjAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB\
+ACTZrmaoV1X1nkR7 / PtvuG7jIqBGOaFkjMSid6HDMkTy4qCvHPCB / 0X6 / 7Ti2PhR\
+HU7 + dYmyDj7 / 8nfQanjfI23SYqhMWGsqzqRvXOZWBlTLfPGDOM01dOiuGGU / gjtB\
+XcuvV8qOLghTJjjb11ye5YrLdd7RcnN + yRaQV3Z6OhDhuzNDoT83gLn1AuqzTVgA\
+YXWhc / O1OqvYtxS3bMKKhSbo6bRTTnOgnBFKpye9VwKx5rNc6RD8ztAScHiZAR3p\
+xN8YaXAdomDJR4HZpXcJ5gCy3rT + 5PvfaCNZ / pKfF8RFbgr / VulmsjspglDLa6t0\
+4VaP + Pxcgr4AjEii2yxksGg=\
+----- END CERTIFICATE-----\
+'
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
@@ -78,7 +78,7 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+  return verify(token,cert, {algorithms: ['RS256']}) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
